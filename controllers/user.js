9fd53dd639d26e11087e9eb322351e256df4d0e4
imranahanger <imranahanger@gmail.com>
2020-04-02 10:36:15 +0530

    usersign and signOut, jwt added

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,42 @@
 const User = require('./../models/user')
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt')
 const {errorHandler} = require('./../helper/dbErrorHandler')
+
 exports.signUp = async (req, res) => {
-   // console.log(req.body)
     const user = new User(req.body)
-    user.save((err,user)=>{
-        if(err){
+    user.save((err, user) => {
+        if (err) {
             return res.status(400).json({
-                err:errorHandler(err)
+                err: errorHandler(err)
             })
         }
-        user.hashed_password=undefined
-        user.salt=undefined
+        user.hashed_password = undefined
+        user.salt = undefined
         res.json({user})
     })
+}
+
+exports.signIn = async (req, res) => {
+    const {email, password} = req.body
+    User.findOne({email}, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User with that email does not exists. please signUp"
+            })
+        }
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            })
+        }
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+        res.cookie('t', token, {expire: new Date() + 10000})
+        const {_id, name, email, role} = user
+        return res.json({token, user: {_id, name, email, role}})
+    })
+}
+exports.signOut = async(req,res)=>{
+    res.clearCookie('t')
+    res.json({message:"SignOut successful"})
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {signUp} = require('./../controllers/user.js')
+const {signUp,signIn,signOut} = require('./../controllers/user.js')
 const {userSignUpValidator} = require('./../validator')
+
 router.post('/signUp',userSignUpValidator, signUp)
+router.post('/signIn', signIn)
+router.post('/signOut', signOut)
 
 module.exports = router
